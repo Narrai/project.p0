9ad0b1ce4ad2594bf03529daadea91c750bac6b3
Nar <rohini.smi2010@gmail.com>
2020-12-17 16:32:28 -0600

    Adding serialization

---
+++ PizzaWorld.Client/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using PizzaWorld.Domain.Models;
+using PizzaWorld.Domain.Singletons;
+
+namespace PizzaWorld.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cs = ClientSingleton.Instance;
+            cs.MakeAStore();
+            // PringAllStores();
+        }
+        static IEnumerable<Store> GetAllStores()
+        {
+            return new List<Store>()
+            {
+                new Store(), new Store()
+            };
+
+        }
+        static void PringAllStores(){
+            foreach(var store in GetAllStores())
+            {   
+                Console.WriteLine(store);
+
+            }
+        }
+    }
+}
